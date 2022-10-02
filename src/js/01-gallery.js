120c14e6f45f28185e388d930479329a074a55b9
Anton Taran <91492704+ToshaUA@users.noreply.github.com>
2022-10-02 15:31:48 +0300

    task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -11,17 +11,16 @@ const imgMarkup = createImgMarkup(galleryItems);
 function createImgMarkup(item) {
   return galleryItems
     .map(({ original, preview, description }) => {
-      return `<a class="gallery__link" href="${original}">
+      return `<a class="gallery__link" 
+      href="${original}">
     <img
       class="gallery__image"
       src="${preview}"
-      alt="${description}"
-    />
-  </a>`;
+      alt="${description}"/>
+    </a>`;
     })
     .join('');
 }
-
 refs.gallery.insertAdjacentHTML('beforeend', imgMarkup);
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
