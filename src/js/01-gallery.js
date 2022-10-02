36a9ec0dcbbdce5decdae575bff5e961502b35ae
Anton Taran <91492704+ToshaUA@users.noreply.github.com>
2022-10-02 14:43:35 +0300

    Exercise_1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,30 @@
-// Add imports above this line
-import SimpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
-// Change code below this line
-console.log(SimpleLightbox);
-console.log(galleryItems);
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from 'simplelightbox';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const imgMarkup = createImgMarkup(galleryItems);
+
+function createImgMarkup(item) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `<a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join('');
+}
+
+refs.gallery.insertAdjacentHTML('beforeend', imgMarkup);
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionType: 'alt',
+});
